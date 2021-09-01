4352bfeaf98b63dfed7b3178e46959e4f5b93990
baptiste0928 <22115890+baptiste0928@users.noreply.github.com>
2021-09-01 16:10:44 +0200

    Setup tests crate

---
+++ rosetta-test/build.rs
@@ -0,0 +1,9 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    rosetta_build::config()
+        .source("fr", "locales/fr.json")
+        .source("en", "locales/en.json")
+        .fallback("en")
+        .generate()?;
+
+    Ok(())
+}
