bc996bd25790c58fbf719af171ddea8f1f40262a
baptiste0928 <22115890+baptiste0928@users.noreply.github.com>
2021-10-17 16:11:51 +0200

    Implement Language for generated types

--- rosetta-test/src/lib.rs
+++ rosetta-test/src/lib.rs
@@ -5,8 +5,24 @@
 
 #[cfg(test)]
 mod tests {
+    use std::{fmt::Debug, hash::Hash};
+
+    use rosetta_i18n::{Language, LanguageId};
+    use static_assertions::assert_impl_all;
 
     rosetta_i18n::include_translations!();
+
+    assert_impl_all!(
+        Lang: Language,
+        Debug,
+        Clone,
+        Copy,
+        Eq,
+        PartialEq,
+        Hash,
+        Send,
+        Sync
+    );
 
     #[test]
     fn test_simple() {
@@ -22,6 +38,24 @@ mod tests {
 
     #[test]
     fn test_fallback() {
-        assert_eq!(Lang::Fr.fallback(), Lang::En.fallback());
+        assert_eq!(Lang::Fr.fallback_key(), Lang::En.fallback_key());
+        assert_eq!(Lang::fallback(), Lang::En);
+    }
+
+    #[test]
+    fn test_from_language_id() {
+        let en = LanguageId::new("en");
+        let fr = LanguageId::new("fr");
+        let de = LanguageId::new("de");
+
+        assert_eq!(Lang::from_language_id(&en), Some(Lang::En));
+        assert_eq!(Lang::from_language_id(&fr), Some(Lang::Fr));
+        assert_eq!(Lang::from_language_id(&de), None);
+    }
+
+    #[test]
+    fn test_to_language_id() {
+        assert_eq!(Lang::En.language_id().value(), "en");
+        assert_eq!(Lang::Fr.language_id().value(), "fr");
     }
 }
